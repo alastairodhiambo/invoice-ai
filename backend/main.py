5e88600d78e6e56988e376823fe95a423ba0a35f
Alastair Odhiambo <alastairodhiambo@outlook.com>
2023-07-01 11:49:24 -0400

    add updates

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile
 
 from invoice import extract_invoice
+# from to_csv import json_to_csv
 
 app = FastAPI()
 
@@ -12,5 +13,7 @@ def read_root():
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-    data = extract_invoice(file.file)
+    data = extract_invoice(file.file).replace('\n', '').replace("'", '"')
+    # json_to_csv(data)
+
     return {"data": data}
